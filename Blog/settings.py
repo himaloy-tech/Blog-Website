1536f4edf6f309a398e0de0914884e5211d1193f
Himaloy <himaloymondal100@gmail.com>
2020-10-18 10:31:47 +0530

    None

--- Blog/settings.py
+++ Blog/settings.py
@@ -75,15 +75,54 @@ WSGI_APPLICATION = 'Blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'HOST':'/cloudsql/my-blog-website-292208:us-west2:blog-website',
-        'NAME':'main',
-        'USER':'Himaloy',
-        'PASSWORD': 'Ira5432@',
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'HOST':'/cloudsql/my-blog-website-292208:us-west2:blog-website',
+#         'NAME':'main',
+#         'USER':'Himaloy',
+#         'PASSWORD': 'Ira5432@',
+#     }
+# }
+if os.getenv('GAE_APPLICATION', None):
+    # Running on production App Engine, so connect to Google Cloud SQL using
+    # the unix socket at /cloudsql/<your-cloudsql-connection string>
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'HOST': '/cloudsql/my-blog-website-292208:us-west2:blog-website',
+            'USER': 'Himaloy',
+            'PASSWORD': 'Ira5432@',
+            'NAME': 'main',
+        }
     }
-}
+else:
+    # Running locally so connect to either a local MySQL instance or connect to
+    # Cloud SQL via the proxy. To start the proxy via command line:
+    #
+    #     $ cloud_sql_proxy -instances=[INSTANCE_CONNECTION_NAME]=tcp:3306
+    #
+    # See https://cloud.google.com/sql/docs/mysql-connect-proxy
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'HOST': '127.0.0.1',
+            'PORT': '5432',
+            'NAME': 'main',
+            'USER': 'Himaloy',
+            'PASSWORD': 'Ira5432@',
+        }
+    }
+# [END db_setup]
+
+# Use a in-memory sqlite3 database when testing in CI systems
+if os.getenv('TRAMPOLINE_CI', None):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+        }
+    }
 
 
 # Password validation
